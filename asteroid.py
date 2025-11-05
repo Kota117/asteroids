74062a13be949dbed8fa8bd4a69dd4ec4c2c30cd
Kota117 <dakotah1375@gmail.com>
2025-11-04 18:24:00 -0700

    starting asteroids class

---
+++ asteroid.py
@@ -0,0 +1,9 @@
+import pygame
+from circleshape import CircleShape
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", (self.x, self.y), self.radius, width=2)
