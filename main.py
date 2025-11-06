45c57ba80d05c4ec40888c741dc8a5eaa34db07d
Kota117 <dakotah1375@gmail.com>
2025-11-05 20:45:59 -0700

    shots kill asteroids

--- main.py
+++ main.py
@@ -43,6 +43,11 @@ def main():
                 log_event("player_hit")
                 print("Game Over!")
                 sys.exit()
+            for shot in shots:
+                if shot.collide(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
 
         screen.fill("black")
         for each in drawable:
